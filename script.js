d7ea8cbc1839a9076751228917ff9370aa9416ec
Mramor11 <167078983+Mramor11@users.noreply.github.com>
2024-09-06 15:20:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ document.getElementById('calculator-form').addEventListener('submit', function(e
 
     if (!isNaN(weight) && !isNaN(reps)) {
         const oneRepMax = weight * (1 + (reps / 30));
-        document.getElementById('result').innerText = `Your estimated 1 Rep Max is: ${oneRepMax.toFixed(2)} kg.`;
+        document.getElementById('result').innerText = `Your estimated 1 RM: ${oneRepMax.toFixed(2)} kg`;
     } else {
-        document.getElementById('result').innerText = 'Please enter valid numbers.';
+        document.getElementById('result').innerText = 'Please enter valid numbers';
     }
 });
